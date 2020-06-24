a681ec1b18f2f9f66e48fe0b02882f679cb10136
Kukuh KKH <Krahmadani1@gmail.com>
2020-06-24 10:56:17 +0700

    Soal PHP Bagian 3

---
+++ Week 2/PHP Bagian 3/papan-catur.php
@@ -0,0 +1,32 @@
+<?php
+
+function papan_catur($angka) {
+   
+}
+
+// TEST CASES
+echo papan_catur(4) 
+/*
+# # # #
+ # # #
+# # # #
+ # # #
+ */
+
+echo papan_catur(8)
+/* 
+# # # # # # # #
+ # # # # # # # 
+# # # # # # # #
+ # # # # # # # 
+# # # # # # # #
+*/
+
+echo papan_catur(5) 
+/*
+# # # # #
+ # # # #
+# # # # #
+ # # # # 
+# # # # #
+*/
